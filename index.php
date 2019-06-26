5ec8a32e46ee1e491d5d55fa2c02a0806471321a
Quintero Deivy <deivy.u@hotmail.com>
2019-06-26 12:06:19 -0400

    hola

---
+++ index.php
@@ -0,0 +1,24 @@
+
+<?php include_once('layouts/header.php'); ?>
+    
+<div class="login-page">
+    <div class="text-center">
+       <h1>Bienvenido</h1>
+       <p>Iniciar sesión </p>
+     </div>
+      <form method="post" action="auth.php" class="clearfix">
+        <div class="form-group">
+              <label for="username" class="control-label">Usuario</label>
+              <input type="name" class="form-control" name="username" placeholder="Usuario">
+        </div>
+        <div class="form-group">
+            <label for="Password" class="control-label">Contraseña</label>
+            <input type="password" name= "password" class="form-control" placeholder="Contraseña">
+        </div>  
+        <div class="form-group">
+                <button type="submit" class="btn btn-info  pull-right">Entrar</button>
+        </div>
+    </form>
+</div>
+   
+<?php include_once('layouts/footer.php'); ?>
